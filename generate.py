3de4fb22b0c69a3848b9e27e85b018181b811e79
OstapchukMixail <37513840+OstapchukMixail@users.noreply.github.com>
2018-03-25 22:40:53 +0300

    Update generate.py
    разбил по функциям, использую with open

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import random
 import argparse
 import os
 import sys
+import pickle
 
 
 parser = argparse.ArgumentParser()
@@ -10,44 +11,44 @@ parser.add_argument('--seed', default='***', dest='First_Word', type=str, help='
 parser.add_argument('--output', default=sys.stdout, dest='output_file', help='this is the address of out')
 parser.add_argument('--length', dest='Length', type=int, help='this is the length of text')
 
-args = parser.parse_args()
 
-# с помощью модели воссоздаю словарь словарей
-file = open(args.model_file, 'r')
-dic2 = {}
-for line in file:
-    tmp = list(line.split(' : '))
-    tmp2 = list(tmp[1].split())
-    dic2[tmp[0]] = {}
-    for symb in tmp2:
-        tmp3 = list(symb.split('*'))
-        dic2[tmp[0]][tmp3[0]] = int(tmp3[1])
-file.close()
-# print(dic2)
+def model_in(args):
 
-# считываю длину текста и первое слово
-Count = args.Length
-str = args.First_Word
-if args.First_Word == '***':
-    List = []
-    for symbol in dic2:
-        List.append(symbol)
-    str = random.choice(List)
+    # с помощью модели воссоздаю словарь словарей
+    dic2 = {}
+    with open(args.model_file, 'rb') as file:
+        dic2 = pickle.load(file)
+    return dic2
 
-file = open(args.output_file, 'w')
 
-# генерирую текст
-file.write(str)
-file.write(' ')
-while Count > 0 and str in dic2:
-    List = []
-    for keys, items in dic2[str].items():
-        for i in range(items):
-            List.append(keys)
-    tmp = random.choice(List)
-    file.write(tmp)
+def In_Generation_Out(args, dic2):
+
+    # считываю длину текста и первое слово
+    Count = args.Length
+    str = args.First_Word
+    if args.First_Word == '***':
+        List = []
+        for symbol in dic2:
+            List.append(symbol)
+        str = random.choice(List)
+    file = open(args.output_file, 'w')
+
+    # генерирую текст
+    file.write(str)
     file.write(' ')
-    Count -= 1
-    str = tmp
-file.write('\n')
-file.close()
+    while Count > 0 and str in dic2:
+        List = []
+        for keys, items in dic2[str].items():
+            for i in range(items):
+                List.append(keys)
+        tmp = random.choice(List)
+        file.write(tmp)
+        file.write(' ')
+        Count -= 1
+        str = tmp
+    file.write('\n')
+    file.close()
+
+
+args = parser.parse_args()
+In_Generation_Out(args, model_in(args))
