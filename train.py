5060c9a7800cfc1668a45f28cb218f4e7e6a6b0d
OstapchukMixail <37513840+OstapchukMixail@users.noreply.github.com>
2018-03-25 22:43:59 +0300

    Update train.py
    Разбил по функциям, использую библиотеку pickle, теперь беру файлы из папки, адрес которой получаю

--- train.py
+++ train.py
@@ -2,56 +2,43 @@ import re
 import argparse
 import os
 import sys
+import pickle
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', dest='output_file', help='This is the address of model')
-parser.add_argument('--input-dir', nargs='?', default=sys.stdin, dest='input_file', type=str, help='this is the address of input')
-parser.add_argument('-ls', nargs='?', default=True, help='Low the text')
+parser.add_argument('--input-dir', nargs='?', default=sys.stdin, dest='input_path', type=str, help='this is the address of input')
+parser.add_argument('--lc', nargs='?', default=True, help='Low the text')
+
+
+def creation(args):
+    dic = {}
+    List = os.listdir(path=args.input_path)
+    for files in List:
+        with open(os.path.join(args.input_path, files), 'r') as file:    # соединяю путь к файлу и путь к его директории
+            last_word = '0'
+            for line in file:
+                List = re.compile(u'[а-яa-zA-ZА-Я0-9-]+|[.,:;?!]+').findall(line)
+                for word in List:
+                    if not args.lc:
+                        word = word.lower()
+                    if last_word == '0':
+                        dic[word] = {}
+                    else:
+                        if word not in dic[last_word]:
+                            dic[last_word][word] = 1
+                        else:
+                            dic[last_word][word] += 1
+                    if word not in dic:
+                        dic[word] = {}
+                    last_word = word
+    return dic
+
+
+def model_out(args, dic):
+    with open(args.output_file, 'wb') as file:
+        pickle.dump(dic, file)
 
 
 args = parser.parse_args()
-
-# создаю словарь словарей из полученного текста
-
-file = open(args.input_file, 'r')
-dic = {}
-last_word = '0'
-for line in file:
-    List = re.compile(u'[а-яa-zA-ZА-Я0-9-]+|[.,:;?!]+').findall(line)
-    for word in List:
-        if not args.ls:
-            word = word.lower()
-        if last_word == '0':
-            dic[word] = {}
-            # print("1 ", dic)
-        else:
-            if word not in dic[last_word]:
-                dic[last_word][word] = 1
-                # print("2 ", dic)
-            else:
-                dic[last_word][word] += 1
-                # print("3 ", dic)
-        if word not in dic:
-            dic[word] = {}
-        last_word = word
-# print(dic)
-file.close()
-
-# создаю модель и записываю её в файл
-file = open(args.output_file, 'w')
-for symb in dic:
-    file.write(symb)
-    file.write(' : ')
-    for subsymb in dic[symb]:
-        file.write(subsymb)
-        file.write('*')
-        file.write(str(dic[symb][subsymb]))
-        file.write(' ')
-    file.write('\n')
-file.close()
-
-
-
-
-
+model_out(args, creation(args))
