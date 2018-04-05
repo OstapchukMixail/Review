5a62ecdd6c967f25775d3e621dc41daf544339f4
OstapchukMixail <37513840+OstapchukMixail@users.noreply.github.com>
2018-04-05 20:07:05 +0300

    Update-нул train.py

--- train.py
+++ train.py
@@ -3,21 +3,22 @@ import argparse
 import os
 import sys
 import pickle
-import collections
-
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', dest='output_file', help='This is the address of model')
 parser.add_argument('--input-dir', nargs='?', default=sys.stdin, dest='input_path', type=str, help='this is the address of input')
 parser.add_argument('--lc', nargs='?', default=True, help='Low the text')
 
+def dd():
+    return defaultdict(int)
+
 
 def creation(args):
-    dic = collections.defaultdict(dict)
+    dic = defaultdict(dd)
     List = os.listdir(path=args.input_path)
     for files in List:
         with open(os.path.join(args.input_path, files), 'r') as file:    # соединяю путь к файлу и путь к его директории
-            last_word = 'something_that_isnot_important'
             flag_lw = False
             for line in file:
                 List = re.compile(u'[а-яa-zA-ZА-Я0-9-]+|[.,:;?!]+').findall(line)
@@ -25,10 +26,7 @@ def creation(args):
                     if not args.lc:
                         word = word.lower()
                     if flag_lw:
-                        if word not in dic[last_word]:
-                            dic[last_word][word] = 1
-                        else:
-                            dic[last_word][word] += 1
+                        dic[last_word][word] += 1
                     else:
                         flag_lw = True
                     last_word = word
